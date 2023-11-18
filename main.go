a87a81ecf9d01c7f07ebfd21b9c107eb922be85e
ayushrakesh <ayushrakesh35711artlover@gmail.com>
2023-11-18 09:42:30 +0530

    Understanding embedded structs

--- main.go
+++ main.go
@@ -2,26 +2,40 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
+
+	// david := person{
+	// 	"David", "Warner",
+	// }
+
+	// var steve person
+
+	// steve.firstName = "Steve"
+	// steve.lastName = "Smith"
+
+	// fmt.Printf("%+v", steve)
+	// fmt.Printf("%+v", david)
+	// fmt.Printf("%+v", kane)
+
 	kane := person{
-		firstName: "Kane", lastName: "Williamson",
+		firstName: "Kane",
+		lastName:  "Williamson",
+		contact: contactInfo{
+			email:   "kane@gmail.com",
+			zipCode: 123456,
+		},
 	}
 
-	david := person{
-		"David", "Warner",
-	}
-
-	var steve person
-
-	steve.firstName = "Steve"
-	steve.lastName = "Smith"
-
-	fmt.Printf("%+v", steve)
-	fmt.Printf("%+v", david)
 	fmt.Printf("%+v", kane)
 }
